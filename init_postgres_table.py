621b4936d30b8d66b2c2e540f8120bac4bceaaaa
Zherebtsov Kirill <kir.ze.2@mail.ru>
2021-04-18 23:21:35 +0300

    Скрипт делает скр-скр

--- init_postgres_table.py
+++ init_postgres_table.py
@@ -4,35 +4,50 @@ from psycopg2 import Error
 
 cur = None # Cursor
 con = None # Connection
-def connect():
-  con = psycopg2.connect(
-    database="schedule", 
-    user="postgres", 
-    password="superpassword", 
-    host="localhost", 
-    port="5432"
-  )
-  cur = con.cursor()
-  print("Database opened successfully, cursor is now in 'cur' varible")
+def connect(): # Соединение с базой
+    global con
+    con = psycopg2.connect(
+        database="schedule", 
+        user="postgres", 
+        password="superpassword", 
+        host="localhost", 
+        port="5432"
+    )
+    global cur
+    cur = con.cursor()
+    print("Database opened successfully, cursor is now in 'cur' varible")
 
 
-def init_table():
-  cur.execute('''CREATE TABLE SCHEDULE  
+def init_table(): # Инициализация таблицы
+    cur.execute('''CREATE TABLE SCHEDULE  
             (ID INT PRIMARY KEY NOT NULL,
             GRP TEXT,
             DAY TEXT,
             LESSON TEXT,
             TYPE TEXT,
             AUDIT TEXT,
-            EVEN TEXT);''') # Вариант 2
-  print("Successfully created table Schedule")
+            ORD INT,
+            EVEN TEXT,
+            WEEK INT[]);''') 
+    print("Successfully created table Schedule")
 
-def end():
+def clear_table(): # Отчистка таблицы
+    cur.execute('DELETE FROM SCHEDULE *')
+    print("Successfully cleared table Schedule")
+
+def delete_table(): # Удаление таблицы
+    cur.execute('DROP TABLE SCHEDULE')
+    print("Successfully deleted table Schedule")
+
+def end(): # Закрытие соединения
     con.commit()
     con.close()
 
 
 if __name__ == "__main__":
     connect()
-    init_table()
+    clear_table()
+    # init_table()
+
+    # delete_table()
     end()
